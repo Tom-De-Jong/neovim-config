072b6c9e65e9b0d35acb6972dde289a8fc81d7f5
Tom de Jong <tomdejong2009@gmail.com>
2026-05-15 21:44:02 +0200

    started project and added files

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration here (optional)
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
